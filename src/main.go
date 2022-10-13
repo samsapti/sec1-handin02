da1562278dcfe84198c4fae6374d9a05f64c0565
Sam Al-Sapti <sals@itu.dk>
2022-10-13 13:58:55 +0200

    Setup gRPC connection

--- src/main.go
+++ src/main.go
@@ -15,6 +15,7 @@ import (
 var (
 	commitment    *pb.Commitment    = &pb.Commitment{}
 	commitmentKey *pb.CommitmentKey = &pb.CommitmentKey{}
+	name          *string           = flag.String("name", "Player_0", "Name of the player")
 	ownPort       *int              = flag.Int("port", 50051, "gRPC port")
 	peerAddr      *string           = flag.String("peer_addr", "localhost:50052", "Peer's gRPC port")
 )
@@ -24,13 +25,13 @@ type server struct {
 }
 
 func (s *server) SendCommitment(ctx context.Context, in *pb.Commitment) (*pb.Empty, error) {
-	log.Printf("Sending commitment: %v", in.GetC())
+	log.Printf("%v receives commitment: %v", *name, in.GetC())
 	commitment = &pb.Commitment{C: in.GetC()}
 	return &pb.Empty{}, nil
 }
 
 func (s *server) SendCommitmentKey(ctx context.Context, in *pb.CommitmentKey) (*pb.Empty, error) {
-	log.Printf("Sending commitment key: (m: %v, r: %v)", in.GetM(), in.GetR())
+	log.Printf("%v receives commitment key: (m: %v, r: %v)", *name, in.GetM(), in.GetR())
 	commitmentKey = &pb.CommitmentKey{M: in.GetM(), R: in.GetR()}
 	return &pb.Empty{}, nil
 }
@@ -38,6 +39,7 @@ func (s *server) SendCommitmentKey(ctx context.Context, in *pb.CommitmentKey) (*
 func main() {
 	flag.Parse()
 
+	// Setup secure channel
 	//altsClient := alts.NewClientCreds(alts.DefaultClientOptions())
 	conn, err := grpc.Dial(*peerAddr, grpc.WithInsecure() /*grpc.WithTransportCredentials(altsClient)*/)
 	if err != nil {
@@ -57,16 +59,20 @@ func main() {
 		}
 	}()
 
+	// Wait for peer to come online
+	time.Sleep(2 * time.Second)
+
+	// Connect to peer
 	client := pb.NewDiceGameClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
-	log.Println("Alice sends commitment")
-	_, e := client.SendCommitment(ctx, &pb.Commitment{C: 235})
+	log.Printf("%v sends commitment: %v", *name, *ownPort)
+	_, e := client.SendCommitment(ctx, &pb.Commitment{C: uint32(*ownPort)})
 	if e != nil {
 		log.Fatalf("Error: %v", e)
 	}
 
 	time.Sleep(time.Second)
-	log.Printf("Bob receives commitment from Alice: %v\n", commitment.C)
+	log.Printf("%v did receive commitment: %v\n", *name, commitment.C)
 }
