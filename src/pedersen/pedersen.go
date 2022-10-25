e1490ef4f75d120468e68a52a4bd878f3b6b5361
Sam Al-Sapti <sals@itu.dk>
2022-10-25 22:49:22 +0200

    Fix Pedersen commitments

--- src/pedersen/pedersen.go
+++ src/pedersen/pedersen.go
@@ -12,6 +12,17 @@ const (
 	h uint64 = 426
 )
 
+func pow(x uint64, y uint64) uint64 {
+	bigX := big.NewInt(int64(x))
+	bigY := big.NewInt(int64(y))
+	bigP := big.NewInt(int64(p))
+
+	var power big.Int
+	power.Exp(bigX, bigY, bigP)
+
+	return power.Uint64()
+}
+
 func GetR() uint64 {
 	r, err := rand.Int(rand.Reader, big.NewInt(int64(p-1)))
 	if err != nil {
@@ -22,7 +33,7 @@ func GetR() uint64 {
 }
 
 func GetCommitment(m uint64, r uint64) uint64 {
-	return (g ^ m) * (h ^ r) % p
+	return pow(g, m) * pow(h, r) % p
 }
 
 func ValidateCommitment(c uint64, m uint64, r uint64) bool {
